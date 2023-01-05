f2265ed5156fcdcf174391c9c172dffd423f8fea
mynameisonoda <tyleronodera@gmail.com>
2023-01-04 17:57:33 -0800

    Finished server file and working on script using codebuddy's script file as template

--- server/server.js
+++ server/server.js
@@ -10,9 +10,43 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// set up a response to let user know that the server is working (status is ok)
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: "Hello from Vizard",
+    })
+});
+
 const response = await openai.createImage({
     prompt: "a white siamese cat",
     n: 1,
     size: "1024x1024",
 });
 image_url = response.data.data[0].url;
+
+
+app.post("/", async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createImage({
+            prompt: `${prompt}`,
+            n: 1,
+            size: "1024x1024",
+        });
+        image_url = response.data.data[0].url;
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error })
+    }
+})
+
+app.listen(5000, () => console.log("Server running on port http://localhost:5000"));
